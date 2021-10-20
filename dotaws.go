49071d90ad4e7cec55f0d0a7c138e1a2776d0803
Juha Ylitalo <juha.ylitalo@reaktor.com>
2021-10-20 18:33:56 +0300

    [dotaws] Make code more compact

--- dotaws.go
+++ dotaws.go
@@ -25,38 +25,33 @@ func credentialsFile() (string, error) {
 	case os.IsNotExist(err):
 		return fname, fmt.Errorf("%s does not exist", fname)
 	default:
-		return fname, fmt.Errorf("failed to stat %s", fname)
+		return fname, fmt.Errorf("failed to check %s", fname)
 	}
 }
 
 func NewDotAws() (*dotAws, error) {
-	fname, err := credentialsFile()
-	if err != nil {
+	if fname, err := credentialsFile(); err != nil {
 		return nil, err
-	}
-	iniFile, err := ini.Load(fname)
-	if err != nil {
+	} else if iniFile, err := ini.Load(fname); err != nil {
 		return nil, err
+	} else {
+		return &dotAws{
+			filename: fname,
+			iniFile:  iniFile,
+		}, nil
 	}
-	return &dotAws{
-		filename: fname,
-		iniFile:  iniFile,
-	}, nil
 }
 
 func (client *dotAws) getProfile(accessKeyId string) (*ini.Section, error) {
 	for _, profile := range client.iniFile.Sections() {
 		id, err := profile.GetKey("aws_access_key_id")
-		if err != nil {
-			continue
-		}
-		if id.String() == accessKeyId {
-			log.Debugf("Found %s from %v profile", accessKeyId, profile.Name())
+		if err == nil && id.String() == accessKeyId {
+			log.Infof("Found %s from %v profile", accessKeyId, profile.Name())
 			return profile, nil
 		}
 	}
-	if os.Getenv("SESSION_TOKEN") != "" {
-		return nil, fmt.Errorf("unable to find AWS profile due to SESSION_TOKEN")
+	if os.Getenv("AWS_SESSION_TOKEN") != "" {
+		return nil, fmt.Errorf("unable to find AWS profile due to AWS_SESSION_TOKEN")
 	}
 	return nil, fmt.Errorf("no profile with %s access key id", accessKeyId)
 }
@@ -64,5 +59,8 @@ func (client *dotAws) getProfile(accessKeyId string) (*ini.Section, error) {
 func (client *dotAws) save(profile *ini.Section, accessKey *types.AccessKey) error {
 	profile.Key("aws_access_key_id").SetValue(*accessKey.AccessKeyId)
 	profile.Key("aws_secret_access_key").SetValue(*accessKey.SecretAccessKey)
-	return client.iniFile.SaveTo(client.filename)
+	if err := client.iniFile.SaveTo(client.filename); err != nil {
+		return fmt.Errorf("failed to save %s due to %v", client.filename, err.Error())
+	}
+	return nil
 }
