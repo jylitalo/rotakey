acb54c93286bbb96d58969014230f6bb5240257d
Juha Ylitalo <juha.ylitalo@reaktor.com>
2021-12-26 00:02:45 +0200

    [tests] CreateAccessKey fails scenario

--- aws_iam_mock.go
+++ aws_iam_mock.go
@@ -1,6 +1,9 @@
 package rotakey
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/aws/aws-sdk-go-v2/service/iam/types"
 )
 
@@ -10,6 +13,9 @@ type awsIamMock struct {
 
 func (client awsIamMock) createAccessKey() (*types.AccessKey, error) {
 	accessKeyId, _ := client.callback.accessKeyID()
+	if strings.HasSuffix(accessKeyId, "Z") {
+		return nil, fmt.Errorf("error condition triggered")
+	}
 	accessKeyId = accessKeyId[:len(accessKeyId)-2] + "Z"
 	secretAccessKey := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMN"
 	return &types.AccessKey{
--- rotate_test.go
+++ rotate_test.go
@@ -8,3 +8,14 @@ func TestExecute(t *testing.T) {
 		t.Errorf("Execute failed due to %v", err)
 	}
 }
+
+func newAwsConfigMockWithErr() (AwsConfigIface, error) {
+	return awsConfigMock{accessKey: "AKIABCDEFGHIJKLKMNOZ"}, nil
+}
+
+func TestCreateAccesskeyError(t *testing.T) {
+	err := NewExec().Execute(ExecuteInput{NewAwsConfig: newAwsConfigMockWithErr, NewDotAws: newDotAwsMock})
+	if err == nil {
+		t.Errorf("Execute did't abort due to err")
+	}
+}
