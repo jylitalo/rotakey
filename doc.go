5dc0be47de5ced1106cfc47805ad5f55db0610a0
Juha Ylitalo <jylitalo@iki.fi>
2023-03-06 18:45:43 +0200

    [go] Add go reference and go report card

--- doc.go
+++ doc.go
@@ -1,4 +1,7 @@
 // Package rotakey helps you rotate AWS keys
+// [![Go Reference](https://pkg.go.dev/badge/github.com/jylitalo/rotakey.svg)](https://pkg.go.dev/github.com/jylitalo/rotakey)
+// [![Go Report Card](https://goreportcard.com/badge/github.com/jylitalo/rotakey)](https://goreportcard.com/report/github.com/jylitalo/rotakey)
+//
 // Currently supports awscli default configuration.
 //
 // to build
