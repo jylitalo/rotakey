eabd78baccae9c2fdbcfb02fd3e6c671670ee1f3
Juha Ylitalo <jylitalo@iki.fi>
2023-03-06 18:51:55 +0200

    [README] Add extra empty line as paragraph break

--- doc.go
+++ doc.go
@@ -1,4 +1,5 @@
 // Package rotakey helps you rotate AWS keys
+//
 // [![Go Reference](https://pkg.go.dev/badge/github.com/jylitalo/rotakey.svg)](https://pkg.go.dev/github.com/jylitalo/rotakey)
 // [![Go Report Card](https://goreportcard.com/badge/github.com/jylitalo/rotakey)](https://goreportcard.com/report/github.com/jylitalo/rotakey)
 //
